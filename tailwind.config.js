610e66684d5bcab1c1d14c81713774c393aa0e6f
sadiaaktersherin <sadiaaktersherin@gmai.com>
2025-08-02 23:49:33 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // enables toggling dark mode via a CSS class
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
