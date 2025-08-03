f6b3388b4da6913463bba81964a341325c82eb5d
sadiaaktersherin <sadiaaktersherin@gmail.com>
2025-08-03 08:09:51 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
-// Vite config placeholder
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/ReactPortfolio/', // ✅ Add this line (must match your repo name)
+  plugins: [react()],
+})
